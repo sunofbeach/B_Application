ffd4384c2269d5a41f5e1854dc5418ff92aaec1d
raywind <yaogamestudio@gmail.com>
2015-10-07 20:53:14 -0400

    first version

---
+++ Banking Application/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Banking_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAcct = new System.Windows.Forms.TextBox();
+            this.txtClient = new System.Windows.Forms.TextBox();
+            this.txtBalance = new System.Windows.Forms.TextBox();
+            this.cmdAdd = new System.Windows.Forms.Button();
+            this.cmdUpdate = new System.Windows.Forms.Button();
+            this.cmdRemove = new System.Windows.Forms.Button();
+            this.cmdTrans = new System.Windows.Forms.Button();
+            this.cmdView = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(163, 14);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(407, 202);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Account#";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Client";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 105);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Balance";
+            // 
+            // txtAcct
+            // 
+            this.txtAcct.Location = new System.Drawing.Point(21, 33);
+            this.txtAcct.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtAcct.Name = "txtAcct";
+            this.txtAcct.Size = new System.Drawing.Size(132, 25);
+            this.txtAcct.TabIndex = 4;
+            // 
+            // txtClient
+            // 
+            this.txtClient.Location = new System.Drawing.Point(21, 78);
+            this.txtClient.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtClient.Name = "txtClient";
+            this.txtClient.Size = new System.Drawing.Size(132, 25);
+            this.txtClient.TabIndex = 5;
+            // 
+            // txtBalance
+            // 
+            this.txtBalance.Location = new System.Drawing.Point(21, 123);
+            this.txtBalance.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtBalance.Name = "txtBalance";
+            this.txtBalance.Size = new System.Drawing.Size(132, 25);
+            this.txtBalance.TabIndex = 6;
+            // 
+            // cmdAdd
+            // 
+            this.cmdAdd.Location = new System.Drawing.Point(21, 155);
+            this.cmdAdd.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cmdAdd.Name = "cmdAdd";
+            this.cmdAdd.Size = new System.Drawing.Size(133, 27);
+            this.cmdAdd.TabIndex = 7;
+            this.cmdAdd.Text = "Add Account";
+            this.cmdAdd.UseVisualStyleBackColor = true;
+            this.cmdAdd.Click += new System.EventHandler(this.cmdAdd_Click);
+            // 
+            // cmdUpdate
+            // 
+            this.cmdUpdate.Enabled = false;
+            this.cmdUpdate.Location = new System.Drawing.Point(21, 189);
+            this.cmdUpdate.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cmdUpdate.Name = "cmdUpdate";
+            this.cmdUpdate.Size = new System.Drawing.Size(133, 27);
+            this.cmdUpdate.TabIndex = 8;
+            this.cmdUpdate.Text = "Update Account";
+            this.cmdUpdate.UseVisualStyleBackColor = true;
+            this.cmdUpdate.Click += new System.EventHandler(this.cmdUpdate_Click);
+            // 
+            // cmdRemove
+            // 
+            this.cmdRemove.Enabled = false;
+            this.cmdRemove.Location = new System.Drawing.Point(21, 224);
+            this.cmdRemove.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cmdRemove.Name = "cmdRemove";
+            this.cmdRemove.Size = new System.Drawing.Size(133, 27);
+            this.cmdRemove.TabIndex = 9;
+            this.cmdRemove.Text = "Remove Account";
+            this.cmdRemove.UseVisualStyleBackColor = true;
+            this.cmdRemove.Click += new System.EventHandler(this.cmdRemove_Click);
+            // 
+            // cmdTrans
+            // 
+            this.cmdTrans.Enabled = false;
+            this.cmdTrans.Location = new System.Drawing.Point(163, 223);
+            this.cmdTrans.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cmdTrans.Name = "cmdTrans";
+            this.cmdTrans.Size = new System.Drawing.Size(208, 27);
+            this.cmdTrans.TabIndex = 10;
+            this.cmdTrans.Text = "Make Transaction";
+            this.cmdTrans.UseVisualStyleBackColor = true;
+            this.cmdTrans.Click += new System.EventHandler(this.cmdTrans_Click);
+            // 
+            // cmdView
+            // 
+            this.cmdView.Enabled = false;
+            this.cmdView.Location = new System.Drawing.Point(379, 223);
+            this.cmdView.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cmdView.Name = "cmdView";
+            this.cmdView.Size = new System.Drawing.Size(189, 27);
+            this.cmdView.TabIndex = 11;
+            this.cmdView.Text = "View Transactions";
+            this.cmdView.UseVisualStyleBackColor = true;
+            this.cmdView.Click += new System.EventHandler(this.cmdView_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 262);
+            this.Controls.Add(this.cmdView);
+            this.Controls.Add(this.cmdTrans);
+            this.Controls.Add(this.cmdRemove);
+            this.Controls.Add(this.cmdUpdate);
+            this.Controls.Add(this.cmdAdd);
+            this.Controls.Add(this.txtBalance);
+            this.Controls.Add(this.txtClient);
+            this.Controls.Add(this.txtAcct);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Banking Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAcct;
+        private System.Windows.Forms.TextBox txtClient;
+        private System.Windows.Forms.TextBox txtBalance;
+        private System.Windows.Forms.Button cmdAdd;
+        private System.Windows.Forms.Button cmdUpdate;
+        private System.Windows.Forms.Button cmdRemove;
+        private System.Windows.Forms.Button cmdTrans;
+        private System.Windows.Forms.Button cmdView;
+    }
+}
+
---
+++ Banking Application/Form1.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+/*
+Banking Application
+Sheridan College 2015 by   
+Zachary Auld & Yao Wang
+*/
+
+namespace Banking_Application
+{
+    public partial class Form1 : Form
+    {
+        private enum ControlState { None_Mode, Add_Mode, UpdateRemove_Mode, Transaction_Mode, View_Mode }
+        private SqlConnection mConn = null;
+        private SqlCommand mCmd = null;
+        private string origAcct = "";
+        private double origBal = 0d;
+        private const string mDatabaseName = "Bank.mdf";
+        private string _mConnStr = null;
+        private string mConnStr
+        {
+            get
+            {
+                if (_mConnStr == null)
+                {
+                    string stringdataDir = System.AppDomain.CurrentDomain.BaseDirectory;
+                    if (stringdataDir.EndsWith(@"\bin\Debug\") || stringdataDir.EndsWith(@"\bin\Release\"))
+                    {
+                        stringdataDir = System.IO.Directory.GetParent(stringdataDir).Parent.Parent.FullName;
+                        System.AppDomain.CurrentDomain.SetData("DataDirectory", stringdataDir);
+                    }
+                    _mConnStr = "Data Source=(LocalDB)\\v11.0;AttachDbFilename=" + stringdataDir + "\\" + mDatabaseName + ";Integrated Security=True";
+
+                }
+                return _mConnStr;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Disable Right-click menu for text fields
+            txtAcct.ContextMenu = new System.Windows.Forms.ContextMenu();
+            txtClient.ContextMenu = new System.Windows.Forms.ContextMenu();
+            txtBalance.ContextMenu = new System.Windows.Forms.ContextMenu();
+
+            //Assigning keypress event handlers
+            dataGridView1.Click += dataGridView1_Click;
+            getData();
+            txtAcct.KeyPress += txtAcct_KeyPress;
+            txtClient.KeyPress += txtClient_KeyPress;
+            txtBalance.KeyPress += txtBalance_KeyPress;
+            dataGridView1.KeyDown += dataGridView1_KeyDown;
+
+            dataGridView1.ClearSelection();
+        }
+        void dataGridView1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Down:
+                case Keys.Up:
+                case Keys.PageUp:
+                case Keys.PageDown:
+                case Keys.Return:
+                    e.Handled = true;
+                    break;
+            }
+        }
+        void txtAcct_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Only Allow Numbers to be Entered, and set minimum length(not decided yet)
+            int c = e.KeyChar;
+            int len = ((TextBox)sender).Text.Length;
+            ((TextBox)sender).SelectionStart = len;
+            if (c != 8)
+            {
+                if (c < 48 || c > 57)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+        void txtClient_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+        void txtBalance_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+        void dataGridView1_Click(object sender, EventArgs e)
+        {
+            if (cmdAdd.Text.Equals("Add Account"))
+            {
+                dataGridView1.CurrentRow.Selected = true;
+                txtAcct.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                txtClient.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                txtBalance.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                origAcct = txtAcct.Text;
+                setControlState(ControlState.UpdateRemove_Mode);
+            }
+        }
+
+        private void getData()
+        {
+
+            try
+            {
+                mConn = new SqlConnection(mConnStr);
+                mConn.Open();
+                mCmd = new SqlCommand();
+                mCmd.Connection = mConn;
+                mCmd.CommandText = "SELECT * FROM [tAccounts] ORDER BY [Account#] ASC";
+                SqlDataReader dr = mCmd.ExecuteReader();
+
+                if (dr.HasRows)
+                {
+                    //bind and display
+                    bindingSource1.DataSource = dr;
+                    dataGridView1.DataSource = bindingSource1;
+                    dataGridView1.ClearSelection();
+
+                    //Auto resize column widths
+                    dataGridView1.AutoResizeColumns();
+                }
+                dr.Close();
+                mConn.Close();
+            }
+            catch (SqlException ex)
+            {
+                if (mConn != null)
+                {
+                    mConn.Close();
+                }
+                MessageBox.Show(ex.Message, "Error Reading Data.");
+            }
+        }
+        private void getTData()
+        {
+            try
+            {
+                mConn = new SqlConnection(mConnStr);
+                mConn.Open();
+                mCmd = new SqlCommand();
+                mCmd.Connection = mConn;
+                mCmd.CommandText = "SELECT * FROM [tAccounts] WHERE [Account#] = '" + txtAcct.Text + "' ORDER BY [Account#] ASC";
+                SqlDataReader dr = mCmd.ExecuteReader();
+
+                if (dr.HasRows)
+                {
+                    //bind and display
+                    bindingSource1.DataSource = dr;
+                    dataGridView1.DataSource = bindingSource1;
+                    dataGridView1.ClearSelection();
+
+                    //Auto resize column widths
+                    dataGridView1.AutoResizeColumns();
+                }
+                dr.Close();
+                mConn.Close();
+            }
+            catch (SqlException ex)
+            {
+                if (mConn != null)
+                {
+                    mConn.Close();
+                }
+                MessageBox.Show(ex.Message, "Error Reading Data.");
+            }
+        }
+        private void cmdAdd_Click(object sender, EventArgs e)
+        {
+            //Check button state
+            if (cmdAdd.Text.Equals("Return to Add Mode"))
+            {
+                setControlState(ControlState.Add_Mode);
+                return;
+            }
+
+            if (isValidAcct() && fieldsNotEmpty())
+            {
+                try
+                {
+                    string sql = "INSERT INTO [tAccounts] ([Account#],[Client],[Balance],[Last Transaction]) VALUES ('" + txtAcct.Text + "','" + txtClient.Text + "','" + txtBalance.Text + "','" + DateTime.Now + "')";
+                    if (mConn.State != ConnectionState.Open)
+                    {
+                        mConn.Open();
+                    }
+                    mCmd = new SqlCommand(sql, mConn);
+                    mCmd.ExecuteNonQuery();
+                    MessageBox.Show("Account " + txtAcct.Text + " successfully created for " + txtClient.Text, "Account Added Successfully", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    getData();
+                    clearText();
+                    mConn.Close();
+                }
+                catch (SqlException ex)
+                {
+                    if (mConn != null)
+                    {
+                        mConn.Close();
+                    }
+                    MessageBox.Show(ex.Message, "Invalid Account", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+        private void cmdUpdate_Click(object sender, EventArgs e)
+        {
+            if (cmdUpdate.Text.Equals("Withdraw"))
+            {
+                string sql = "UPDATE [tAccounts] SET [Balance] = '" + (origBal - Convert.ToDouble(txtBalance.Text)) + "', [Last Transaction] = '" + DateTime.Now + "' WHERE [Account#] = '" + txtAcct.Text + "'";
+                try
+                {
+                    mConn.Open();
+                    mCmd.CommandText = sql;
+                    mCmd.ExecuteNonQuery();
+                    mConn.Close();
+                    getTData();
+                }
+                catch (SqlException ex)
+                {
+                    if (mConn != null)
+                    {
+                        mConn.Close();
+                    }
+                    MessageBox.Show(ex.Message, "Error Making Transaction.");
+                }
+            }
+            else
+            {
+                string sql = "UPDATE [tAccounts] SET [Account#] = '" + txtAcct.Text + "', [Client] = '" + txtClient.Text + "', [Balance] = '" + txtBalance.Text + "' WHERE [Account#] = '" + origAcct + "'";
+                try
+                {
+                    mConn.Open();
+                    mCmd.CommandText = sql;
+                    mCmd.ExecuteNonQuery();
+                    mConn.Close();
+                    getData();
+                }
+                catch (SqlException ex)
+                {
+                    if (mConn != null)
+                    {
+                        mConn.Close();
+                    }
+                    MessageBox.Show(ex.Message, "Error Updating Account.");
+                }
+                setControlState(ControlState.Add_Mode);
+            }
+        }
+        private void cmdRemove_Click(object sender, EventArgs e)
+        {
+            if (cmdRemove.Text.Equals("Deposit"))
+            {
+
+            }
+            else
+            {
+                if (MessageBox.Show("Are you sure you want to remove this account?", "Confirm Record Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    string sql = "DELETE FROM [tAccounts] WHERE [Account#] = '" + origAcct + "'";
+                    try
+                    {
+                        mConn.Open();
+                        mCmd.CommandText = sql;
+                        mCmd.ExecuteNonQuery();
+                        mConn.Close();
+                        getData();
+                    }
+                    catch (SqlException ex)
+                    {
+                        if (mConn != null)
+                        {
+                            mConn.Close();
+                        }
+                        MessageBox.Show(ex.Message, "Error Deleting Record.");
+                    }
+                }
+                setControlState(ControlState.Add_Mode);
+            }
+        }
+        private void cmdTrans_Click(object sender, EventArgs e)
+        {
+            setControlState(ControlState.Transaction_Mode);
+        }
+        private void cmdView_Click(object sender, EventArgs e)
+        {
+            setControlState(ControlState.View_Mode);
+        }
+        private void clearText()
+        {
+            txtAcct.Text = "";
+            txtClient.Text = "";
+            txtBalance.Text = "";
+            txtAcct.Focus();
+            dataGridView1.ClearSelection();
+        }
+        private bool isValidAcct()
+        {
+            return true;
+        }
+        private bool fieldsNotEmpty()
+        {
+            //Checks the fields to avoid entering null data
+            if (txtAcct.TextLength != 0 && txtClient.TextLength != 0 && txtBalance.TextLength != 0)
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("All fields must be filled in!", "Missing Account Information", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return false;
+            }
+        }
+        private void setControlState(ControlState state)
+        {
+            switch (state)
+            {
+                case ControlState.Add_Mode:
+                    clearText();
+                    cmdAdd.Text = "Add Account";
+                    cmdUpdate.Text = "Update Account";
+                    cmdRemove.Text = "Remove Account";
+                    cmdView.Text = "View Transactions";
+                    cmdUpdate.Enabled = false;
+                    cmdRemove.Enabled = false;
+                    cmdTrans.Enabled = false;
+                    cmdView.Enabled = false;
+                    txtBalance.Enabled = true;
+                    txtAcct.Enabled = true;
+                    dataGridView1.ClearSelection();
+                    getData();
+                    break;
+                case ControlState.UpdateRemove_Mode:
+                    cmdAdd.Text = "Return to Add Mode";
+                    cmdUpdate.Enabled = true;
+                    cmdRemove.Enabled = true;
+                    cmdTrans.Enabled = true;
+                    cmdView.Enabled = true;
+                    txtAcct.Enabled = true;
+                    txtClient.Enabled = true;
+                    txtBalance.Enabled = false;
+                    break;
+                case ControlState.Transaction_Mode:
+                    cmdAdd.Text = "Return to Add Mode";
+                    cmdUpdate.Text = "Withdraw";
+                    cmdRemove.Text = "Deposit";
+                    txtAcct.Enabled = false;
+                    txtBalance.Enabled = true;
+                    origBal = Convert.ToDouble(txtBalance.Text);
+                    txtBalance.Text = "";
+                    cmdTrans.Enabled = false;
+                    cmdView.Enabled = true;
+                    getTData();
+                    break;
+                case ControlState.View_Mode:
+                    clearText();
+                    txtAcct.Enabled = false;
+                    txtClient.Enabled = false;
+                    txtBalance.Enabled = false;
+                    cmdUpdate.Enabled = false;
+                    cmdRemove.Enabled = false;
+                    cmdTrans.Enabled = true;
+                    cmdView.Enabled = false;
+                    break;
+                case ControlState.None_Mode:
+                default:
+                    break;
+            }
+        }
+    }
+}
